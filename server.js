7471594346fa4ab15a3f0006fdd73f22591fc962
Madlen <zhotheone@gmail.com>
2025-04-08 16:16:47 +0000

    0.6 - css

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 // Ініціалізація Express додатку
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 // Middleware
